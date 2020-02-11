3d6ddf65d3df53eacecbdd775928e7276b800509
daijiuhu <daijiuhu@6cloudtech.com>
2020-02-11 11:23:12 +0800

    添加目录

---
+++ src/component/tabs/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="tabs">
+    <el-tabs v-model="activeName"
+             @tab-click="handleClick">
+      <el-tab-pane label="用户管理"
+                   name="first"> </el-tab-pane>
+      <el-tab-pane label="配置管理"
+                   name="second"></el-tab-pane>
+      <el-tab-pane label="角色管理"
+                   name="third"></el-tab-pane>
+      <el-tab-pane label="定时任务补偿"
+                   name="fourth"></el-tab-pane>
+    </el-tabs>
+  </div>
+
+</template>
+<script>
+export default {
+  name: 'tabs',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+    sendFatherPasswordShow () {
+      this.$emit('func', false)
+    }
+  }
+}
+</script>
+<style lang='scss'  scoped>
+@import "./css/index.scss";
+</style>
