7508fbf6decbbce8f2fb59cc0edca84f01a24812
王继山 <1521173717@qq.com>
2020-03-09 20:11:21 +0800

    样式修改

--- src/component/showTipmsg/index.vue
+++ src/component/showTipmsg/index.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    <div class="tipSty" @click="showTip">
-      <i class="el-icon-arrow-left iconSty
-"></i>
+    <div class="tipSty" @click="drawer = true" v-show="!drawer">
+      <i class="el-icon-arrow-left"></i>
+    </div>
+    <div class="tipCloseSty" @click="drawer = false" v-if="drawer">
+      <i class="el-icon-arrow-right"></i>
     </div>
     <el-drawer
       title="提示信息"
+      size="322px"
       :visible.sync="drawer"
-      :wrapperClosable="true"
+      :wrapperClosable="false"
+      :with-header="false"
+      :modal="false"
     >
       <div v-html="tipmsg" class="fontCss">
       </div>
@@ -29,9 +34,7 @@ export default {
     }
   },
   methods: {
-    showTip () {
-      this.drawer = true
-    }
+
   }
 }
 </script>
